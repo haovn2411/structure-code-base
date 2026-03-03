f8c8396c4f28b0a1713fa747e1c94532acd1f52c
hao.vu <hao.vu@hdwebsoft.dev>
2026-03-03 17:48:40 +0700

    git add product init

--- src/Presentation/StructureCodeSolution.API/Controllers/DeviceController.cs
+++ src/Presentation/StructureCodeSolution.API/Controllers/DeviceController.cs
@@ -1,8 +1,10 @@
-﻿using MediatR;
+﻿using Azure;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using StructureCodeSolution.API.Abstractions;
 using StructureCodeSolution.Application.Abstractions.Shared;
 using StructureCodeSolution.Application.Usecases.V1.Commands.Devices.Abstracts;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace StructureCodeSolution.API.Controllers
 {
@@ -23,5 +25,32 @@ namespace StructureCodeSolution.API.Controllers
             }
             return Ok(result);
         }
+        //[HttpGet("{DeviceId}")]
+        //[ProducesResponseType(typeof(Result<Response.DeviceResponse>), StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //public async Task<IActionResult> Devices(Guid DeviceId)
+        //{
+        //    var result = await Sender.Send(new Query.GetDeviceByIdQuery(DeviceId));
+        //    return Ok(result);
+        //}
+
+        //[HttpDelete("{DeviceId}")]
+        //[ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //public async Task<IActionResult> DeleteDevices(Guid DeviceId)
+        //{
+        //    var result = await Sender.Send(new Command.DeleteDeviceCommand(DeviceId));
+        //    return Ok(result);
+        //}
+
+        //[HttpPut("{DeviceId}")]
+        //[ProducesResponseType(typeof(Result), StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //public async Task<IActionResult> Devices(Guid DeviceId, [FromBody] Command.UpdateDeviceCommand updateDevice)
+        //{
+        //    var updateDeviceCommand = new Command.UpdateDeviceCommand(DeviceId, updateDevice.Name, updateDevice.Price, updateDevice.Description);
+        //    var result = await Sender.Send(updateDeviceCommand);
+        //    return Ok(result);
+        //}
     }
 }
